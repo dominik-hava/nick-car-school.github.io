b90f383ea2963703245b15456281dc07b190fcaf
Dominik Háva <78072599+dominik-hava@users.noreply.github.com>
2024-02-01 20:36:20 +0100

    Added a way to remove specific row in the table + fixed some bugs

--- js/script.js
+++ js/script.js
@@ -1,33 +1,48 @@
 const addDataButton = document.getElementById('add-data-button')
 const clearTableButton = document.getElementById('clear-table-button')
 const userInput = document.getElementById('question-number')
+var columnValues = Array.from(document.querySelectorAll('#question-table td:first-child')).map(cell => cell.textContent)
+
 clearTableButton.addEventListener('click', clearTable)
 addDataButton.addEventListener('click', addData)
 userInput.addEventListener('keyup', e => {
     if (e.key === 'Enter') {
         addData()
     }
-}
-)
+})
+
 document.addEventListener('DOMContentLoaded', function (event) {
     loadSavedData();
 });
+
 function clearTable() {
     var table = document.getElementById('question-table').getElementsByTagName('tbody')[0]
     table.innerHTML = ''
+    columnValues = []
     saveToLocalStorage();
 }
+
+function removeItem(row) {
+    var confirmation = confirm(`Are you sure you want to delete question number "${row.cells[0].innerHTML}"?`)
+    if (confirmation) {
+        var table = document.getElementById('question-table').getElementsByTagName('tbody')[0]
+        var rowIndex = row.rowIndex
+        var questionNumber = row.cells[0].innerHTML
+        var index = columnValues.findIndex(value => value === questionNumber)
+        columnValues.splice(index, 1)
+        table.deleteRow(rowIndex-1)
+        saveToLocalStorage()
+    }
+}
+
 function addData() {
     // Get input value
     var questionNumber = userInput.value
-    var columnValues = Array.from(document.querySelectorAll('#question-table td:first-child')).map(cell => cell.textContent)
-    console.log(columnValues)
-    for(let columnValue in columnValues) {
-        if (columnValue === questionNumber) {
-            alert(`Question number "${questionNumber}" already exists`)
-            return
-        }
+    if (columnValues.map(value => value.toLowerCase()).includes(questionNumber.toLowerCase())) {
+        alert(`Question number "${questionNumber}" already exists`)
+        return
     }
+
     // Validate input
     if (!questionNumber) {
         alert('Please enter a question number')
@@ -42,7 +57,14 @@ function addData() {
 
     cell1.innerHTML = questionNumber;
     cell2.innerHTML = '<input type="checkbox">'
+    columnValues.push(questionNumber)
+    userInput.value = ''
     saveToLocalStorage();
+
+    // Add click event listener to the row
+    row.addEventListener('click', function () {
+        removeItem(row)
+    });
 }
 
 function loadSavedData() {
@@ -50,6 +72,15 @@ function loadSavedData() {
     if (savedData) {
         var table = document.getElementById('question-table').getElementsByTagName('tbody')[0]
         table.innerHTML = savedData;
+        columnValues = Array.from(document.querySelectorAll('#question-table td:first-child')).map(cell => cell.textContent)
+
+        // Add click event listener to each row
+        var rows = table.getElementsByTagName('tr');
+        for (var i = 0; i < rows.length; i++) {
+            rows[i].addEventListener('click', function () {
+                removeItem(this);
+            });
+        }
     }
 }
 
@@ -57,4 +88,3 @@ function saveToLocalStorage() {
     var table = document.getElementById('question-table').getElementsByTagName('tbody')[0]
     localStorage.setItem('learning-data', table.innerHTML)
 }
-
